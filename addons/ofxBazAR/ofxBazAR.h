3f90b1527e0a5230d517eab1011271bcae1bca1d
lian <meta.rb@gmail.com>
2009-02-06 13:24:21 +0100

    changed: addons/ofxBazAR (added credits + license info)

--- addons/ofxBazAR/ofxBazAR.h
+++ addons/ofxBazAR/ofxBazAR.h
@@ -1,3 +1,25 @@
+/*
+ 
+ Copyright 2008, 2009 Lian - meta.rb@gmail.com
+ Distributed under the terms of the GNU Lesser General Public License v3
+ 
+ ofxBazAR is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Lesser General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ ofxBazAR is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ ofxBazAR is wraps around the BazAR computer vision library..
+
+ 	BazAR Website: http://cvlab.epfl.ch/software/bazar/
+	BazAR Credits: Julien Pilet, Andreas Geiger, Mustafa Ozuysal, Vincent Lepetit, Andre Mazzoni
+
+ */
+
 #ifndef _OFX_BAZAR_H_
 #define _OFX_BAZAR_H_
 
