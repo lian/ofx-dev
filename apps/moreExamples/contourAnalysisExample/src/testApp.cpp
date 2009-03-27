4338142c12681d4f28b2e07e99b9173d892a6eff
lian <meta.rb@gmail.com>
2009-03-27 03:02:35 +0100

    add apps/moreExamples/contourAnalysisExample  - http://csugrue.com/code/?p=10

---
+++ apps/moreExamples/contourAnalysisExample/src/testApp.cpp
@@ -0,0 +1,117 @@
+#include "testApp.h"
+
+
+//--------------------------------------------------------------
+void testApp::setup(){
+
+    ofBackground(0,0,0);
+
+    image.loadImage(ofToDataPath("images/board_0.jpg"));
+
+    w = image.width;
+    h = image.height;
+
+    imageAsGray.allocate(w,h);
+
+    mouseX = 0;
+    mouseY = 0;
+
+
+    imageAsGray.setFromPixels( image.getPixels(), w, h);
+    imageAsGray.threshold(100);
+    imageAsGray.invert();
+
+
+    int     minArea             = 20;
+    int     maxArea             = w*h*.5;
+    int     nConsidered         = 30;
+    bool    bFindHoles          = false;
+    bool    bUseApproximation   = false;
+
+    contourFinder.findContours(imageAsGray,minArea,maxArea,nConsidered, bFindHoles,bUseApproximation);
+
+
+}
+//--------------------------------------------------------------
+void testApp::update(){
+
+
+
+
+
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::draw(){
+
+
+    ofEnableAlphaBlending();
+
+    ofSetColor(0xffffff);
+    image.draw(0,0);
+
+
+    ofSetColor(0x8aff00);
+    ofNoFill();
+    glLineWidth(2.0);
+
+    for( int i = 0; i < contourFinder.nBlobs; i++)
+    {
+        findLines(contourFinder.blobs[i].pts,lines,30,40,30);
+
+        for( int j = 0; j < lines.size(); j++)
+        {
+            ofLine(lines[j].x,lines[j].y,lines[j].z,lines[j].w);
+            ofCircle(lines[j].x,lines[j].y,3);
+            ofCircle(lines[j].z,lines[j].w,3);
+        }
+
+    }
+
+
+    ofSetColor(0xffffff);
+    ofDrawBitmapString( ofToString( ofGetFrameRate(),2), 40, 460);
+
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyPressed  (int key){
+
+
+
+    switch( key )
+    {
+        default: break;
+    }
+
+}
+
+//--------------------------------------------------------------
+void testApp::keyReleased  (int key){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseMoved(int x, int y )
+{
+
+    mouseX = x;
+    mouseY = y;
+}
+
+//--------------------------------------------------------------
+void testApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void testApp::mouseReleased(){
+}
---
+++ apps/moreExamples/contourAnalysisExample/src/testApp.h
@@ -0,0 +1,42 @@
+#ifndef _TEST_APP
+#define _TEST_APP
+
+
+#include "ofMain.h"
+#include "contourAnalysis.h"
+
+class testApp : public ofSimpleApp{
+
+	public:
+
+		void setup();
+		void update();
+		void draw();
+
+		void keyPressed(int key);
+		void keyReleased(int key);
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased();
+
+        ofImage                 image;
+        ofxCvGrayscaleImage     imageAsGray;
+        ofxCvContourFinder      contourFinder;
+        vector<ofxPoint4f>      lines;
+
+        int w, h;
+        float mouseX,mouseY;
+
+
+
+
+
+
+
+
+
+};
+
+#endif
+
